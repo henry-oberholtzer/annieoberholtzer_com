f2c49ca263bd5bdcf989ad5065cacaf2f42517a1
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-09-24 14:20:02 -0700

    logo updates, nav updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚔️</text></svg>"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='90' font-size='90'>⚔️</text></svg>"
       />
       <body className={`${vollkorn.className}`}>
         {children}
--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -3,8 +3,9 @@ import styles from './logo.module.css'
 const Logo = () => {
   return (
     <a className={styles.box} href="">
-      <h1 className={styles.heading}><span className={styles.capital}>A</span>nnie <span className={styles.capital}>O</span>berholtzer</h1>
-      <p className={styles.subheading}><span>Actress</span><span>⚔️</span><span>Fighter</span></p>
+      <h1 className={styles.heading}><span className={styles.capital}>A</span>nnie&nbsp;<span className={styles.capital}>O</span>berholtzer</h1>
+      <div className={styles.underline}/>
+      <p className={styles.subheading}><span>Actor</span><span>⚔️</span><span>Fighter</span></p>
     </a>
   )
 }
